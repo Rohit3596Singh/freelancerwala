14954f354ef0953fa191eff1694cbca9514302a0
Rohit Singh <srohit3596@gmail.com>
2025-02-06 22:23:05 +0530

    auth validation at backend

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
-// password:- WDtwanKg0vYkv5Er
-// username:- srohit2806
-// Connection string :-  mongodb+srv://<db_username>:<db_password>@cluster0.dx2wa.mongodb.net/
+// password:- pcy4AsakLj1lFNNQ
+// username:- srohit3596
+// Connection string :-  mongodb+srv://srohit3596:pcy4AsakLj1lFNNQ@cluster0.txrii.mongodb.net/
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require ('cors')
 require('dotenv').config();
+require('./models/db')
+const AuthRouter = require('./Routes/AuthRoute')
 
 const PORT = process.env.PORT || 8080;
 
 app.get("/ping",(req,res)=>{
     res.send("PONG");
 })
+
+app.use(bodyParser.json)
+app.use(cors())
+app.use('/auth',AuthRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
