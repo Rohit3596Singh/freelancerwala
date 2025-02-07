2daae3bbf7526dbf9830f8de2029e18f0a54930c
Rohit Singh <srohit3596@gmail.com>
2025-02-07 19:06:12 +0530

    checking api

--- server/index.js
+++ server/index.js
@@ -15,7 +15,15 @@ app.get("/ping",(req,res)=>{
 })
 
 app.use(bodyParser.json())
-app.use(cors())
+
+app.use(cors(
+    {
+        origin: ["http://localhost:5173", "https://freelancerwala-9xw4.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true, // Allow cookies/authentication headers
+      }
+))
+
 app.use('/auth',AuthRouter)
 
 app.listen(PORT,()=>{
