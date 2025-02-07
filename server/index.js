53aad51d6ea39f41c067886005a6d4a75bd85400
Rohit Singh <srohit3596@gmail.com>
2025-02-07 21:40:11 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -1,28 +1,39 @@
 
 
 const express = require('express');
-const app = express();
+
 const bodyParser = require('body-parser')
 const cors = require ('cors')
 require('dotenv').config();
 require('./models/db')
 const AuthRouter = require('./Routes/AuthRoute')
 
+const app = express();
+
 const PORT = process.env.PORT || 8080;
+
+
+
+app.use(cors(
+    
+))
+// app.use(cors(
+//     {
+//         // origin: ["http://localhost:5173","https://freelancerwala-9xw4.vercel.app","*"],
+//         origin: ["https://freelancerwala-9xw4.vercel.app"],
+//         // origin: "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         credentials: true, // Allow cookies/authentication headers
+//       }
+// ))
+
+app.use(bodyParser.json())
 
 app.get("/ping",(req,res)=>{
     res.send("PONG");
 })
 
-app.use(bodyParser.json())
 
-app.use(cors(
-    {
-        origin: ["http://localhost:5173", "https://freelancerwala-9xw4.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true, // Allow cookies/authentication headers
-      }
-))
 
 app.use('/auth',AuthRouter)
 
