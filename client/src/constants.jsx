5e8cffcd7db86e7780870271ebad2a7756bc43f8
Rohit Singh <srohit3596@gmail.com>
2025-02-10 19:55:57 +0530

    ui changes

--- client/src/constants.jsx
+++ client/src/constants.jsx
@@ -1,2 +1,2 @@
-export const BackendUrl = "http://localhost:8080/"
-// export const BackendUrl = "https://freelancerwala.onrender.com/"
+// export const BackendUrl = "http://localhost:8080/"
+export const BackendUrl = "https://freelancerwala.onrender.com/"
